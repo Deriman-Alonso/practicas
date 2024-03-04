e81e46c7ae7611b7c26d77d0435672d3431a3d50
Deriman-Alonso <161326520+Deriman-Alonso@users.noreply.github.com>
2024-03-04 11:41:12 +0000

    Añadiendo la carpeta del módulo

---
+++ sports_association_module/__manifest__.py
@@ -0,0 +1,23 @@
+#  GNU nano 6.2 
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sports Association",
+    "summary": "Manage sports association for test",
+    "version": "16.0.1.0.0",
+    "category": "Sports",
+    "author": "<Deriman Alonso>, Binhex Systems Solutions",
+    # see https://odoo-community.org/page/maintainer-role for a description of the maintainer role and responsibilities
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+
+    "data": [],
+}
+
+
+
+
