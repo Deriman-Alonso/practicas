18dc86a255be9c670dbfe04583359c4be2ce6b50
Deriman-Alonso <d.alonso@binhex.cloud>
2024-03-05 12:13:37 +0000

    [ADD] Sports Issue Model

---
+++ sports_association_module/models/sport_issue.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+class SportIssue(models.Model):
+    _name = "sport.issue"
+    _description = "Model for reporting Sports Issues"
+
+    name = fiels.Char(string='Name')
+    description = fields.Text(string='Description')
+    date = fields.Date(string="Date")
+    assistance = fields.Bool(string="Assistance")
+    state = fields.Selection([('draft','Draft'),('open','Open'),('closed','Closed')],string="State",default="draft")
