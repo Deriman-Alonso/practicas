95803eabcb916cd3e3103e73848204e3640b6f44
Deriman-Alonso <d.alonso@binhex.cloud>
2024-03-05 12:26:07 +0000

    [FIX] Sports Issue Module

--- sports_association_module/models/sport_issue.py
+++ sports_association_module/models/sport_issue.py
@@ -7,5 +7,5 @@ class SportIssue(models.Model):
     name = fields.Char(string='Name')
     description = fields.Text(string='Description')
     date = fields.Date(string="Date")
-    assistance = fields.Bool(string="Assistance")
+    assistance = fields.Boolean(string="Assistance")
     state = fields.Selection([('draft','Draft'),('open','Open'),('closed','Closed')],string="State",default="draft",)
